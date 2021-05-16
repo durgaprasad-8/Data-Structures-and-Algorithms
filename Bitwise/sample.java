1b1961c6c322034fe627755acf775fcbd255f7e3
Durga Prasad <73108375+durgaprasad-8@users.noreply.github.com>
2021-05-16 22:37:05 +0530

    Add files via upload

---
+++ Bitwise/sample.java
@@ -0,0 +1,16 @@
+// sample java program to explore bitmagic like XOR, OR and AND
+
+class sample{
+
+    public static void main(String[] args) {
+        int x = 87;
+        int y = 25;
+
+        System.out.println(x ^ y);
+        System.out.println(x | y);
+        System.out.println(x & y);
+
+        System.out.println(x << 2);
+        System.out.println(y >> 2);
+    }
+}
